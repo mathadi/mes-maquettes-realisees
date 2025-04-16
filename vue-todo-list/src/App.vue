bfe3f4f33e66d1ffbca5fe311cc7d554cc4991f5
Mathieu Adimi <Mathieu.Adimi@chem.loc>
2025-04-16 16:56:18 +0200

    first commit

---
+++ vue-todo-list/src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import AppHeader from './components/AppHeader.vue';
+import AppMain from './components/AppMain.vue';
+import AppFooter from './components/AppFooter.vue';
+
+</script>
+
+<template>
+  <div>
+    <AppHeader></AppHeader>
+
+    <AppMain></AppMain>
+
+    <AppFooter></AppFooter>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ vue-todo-list/src/components/AppFooter.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+</script>
+
+<template>
+    <div>
+        <footer class="footer">
+            <p>&copy; Highfive 2025. Tous droits réservés.</p>
+        </footer>
+    </div>
+</template>
+
+<style scoped></style>
